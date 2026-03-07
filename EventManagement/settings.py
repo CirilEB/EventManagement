2d7b027a6db3cf161da5be6b48fd80f0273d8e83
Ciril <cirileb44@gmail.com>
2026-03-08 02:03:35 +0530

    Increased Protection

--- EventManagement/settings.py
+++ EventManagement/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,8 +131,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "cirileb2003@gmail.com"
-EMAIL_HOST_PASSWORD = "fnjrnjjjmuyxidji"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
