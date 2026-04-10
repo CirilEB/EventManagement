43bf3c7ad64897ab967122ce44f26abbc3d83548
Ciril <cirileb44@gmail.com>
2026-04-10 13:41:41 +0530

    protection

--- EventManagement/settings.py
+++ EventManagement/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ymjd_-pa@tv0uc+p$hs!ja6hoxs!$98lm@b0wnumqk$^5mco4'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
